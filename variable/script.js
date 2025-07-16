c90c43d19feb680f9da1213e1a55730bb3580849
baliram072x <baliramrathod0008@gmail.com>
2025-07-16 08:54:16 +0530

    variables

---
+++ variable/script.js
@@ -0,0 +1,25 @@
+console.log("hey this is my practice");
+
+let x= "bali bro";
+let y= 25;
+let z= 3.55;
+const p=true;
+let q=undefined;
+let r=null;
+
+console.log(x,y,z,p,q,r)
+
+console.log(typeof x,typeof y,typeof z, typeof p,typeof q,typeof r)
+
+let o= {
+    name : "baliram",
+    "job code": 5600 ,
+    is_handsome: true
+}
+console.log(o);
+o.salary="100 crores"
+console.log(o)
+o.salary="500 crores"
+console.log(o)
+
+//alert("hello world")
