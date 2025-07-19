e1223f4d41bf327c09af92b201925baf00437ecd
baliram072x <baliramrathod0008@gmail.com>
2025-07-19 23:03:17 +0530

    strings

---
+++ strings/index.js
@@ -0,0 +1,15 @@
+console.log("this is strings tutorial")
+let a= "Baliram";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+
+console.log(a.length)
+
+let real_friend ="harry"
+let friend ="rohan"
+console.log("his name is "+ real_friend + " his friends name is "+ friend)
+console.log(`his name is ${real_friend} and his friend name is ${friend}`)
